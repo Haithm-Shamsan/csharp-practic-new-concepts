93ec169805149b70920817ff3b8a467bfde08d15
Haithm-Shamsan <Haithm-Shamsan@users.noreply.github.com>
2026-03-26 18:44:30 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practic_new_concepts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void Form2_DataBack(object sender,int PersonID)
+        {
+            textBox1.Text=PersonID.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+           frm.DataBack += Form2_DataBack;
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practic_new_concepts
+{
+    public partial class Form2 : Form
+    {
+        public delegate void DataBackEventHandler(object sender,int PerosnID);
+
+        public event DataBackEventHandler DataBack;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int PersonID =int.Parse( txtPersonID.Text);
+
+            DataBack?.Invoke(this, PersonID);
+            this.Close();
+        }
+    }
+}
